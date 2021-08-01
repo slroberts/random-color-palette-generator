8277d8a6ec6658491d0a236bb96dcc9c2504c37b
Shomari Roberts <shomariroberts@gmail.com>
2021-08-01 01:39:13 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='md:container md:mx-auto flex justify-center'>
+      <h1 className='text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate'>
+        Color Palette Generator
+      </h1>
+    </div>
+  );
+};
+
+export default App;
