d0be375367fe2554b987788ca1c8cfc68c1f1184
Shomari Roberts <shomariroberts@gmail.com>
2021-08-01 21:29:06 -0400

    created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <h1 className='text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate text-center mt-32'>
+      Color Palette Generator
+    </h1>
+  );
+};
+
+export default Header;
