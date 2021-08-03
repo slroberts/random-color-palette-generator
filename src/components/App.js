46ab612f8fbec0be33bd56e3226f6dfb19d9b80a
Shomari Roberts <shomariroberts@gmail.com>
2021-08-03 09:42:40 -0400

    fetch color via api call

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,53 @@ import Header from './Header';
 import ColorCard from './ColorCard';
 import Button from './Button';
 
-const App = () => {
-  return (
-    <div className='md:container md:mx-auto p-4'>
-      <Header />
-      <div className='flex flex-wrap mt-16 justify-around'>
-        <ColorCard />
-        <ColorCard />
-        <ColorCard />
-        <ColorCard />
-        <ColorCard />
+class App extends React.Component {
+  state = {
+    colors: [],
+  };
+
+  fetchColors = () => {
+    (async () => {
+      const data = await fetch('http://colormind.io/api/', {
+        method: 'POST',
+        body: JSON.stringify({
+          model: 'default',
+        }),
+      })
+        .then((result) => result.json())
+        .catch(console.log);
+
+      this.setState({ colors: data.result });
+    })();
+  };
+
+  componentDidMount() {
+    this.fetchColors();
+
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  generateColors = () => {
+    this.fetchColors();
+  };
+
+  handleKeyDown = (event) => {
+    if (event.key === ' ') {
+      this.fetchColors();
+    }
+  };
+
+  render() {
+    return (
+      <div className='md:container md:mx-auto p-4'>
+        <Header />
+
+        <ColorCard colors={this.state.colors} />
+
+        <Button generateColors={this.generateColors} />
       </div>
-      <Button />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
