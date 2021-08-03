0d9b5427576e77e85c36928f6a8f7378beeeee23
Shomari Roberts <shomariroberts@gmail.com>
2021-08-03 09:46:29 -0400

    rendered color cards via the colors prop

--- src/components/ColorCard.js
+++ src/components/ColorCard.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
-const ColorCard = () => {
+const ColorCard = ({ colors }) => {
   return (
-    <div className='mx-1 mb-12 shadow-lg rounded-xl w-3/5 md:w-52 h-56 md:h-72 bg-white'>
-      <div className='bg-blue-800 m-2 w-50 h-40 md:h-56 rounded-md'></div>
-      <div className='text-center font-bold uppercase p-1 text-gray-500'>
-        # Color Code
-      </div>
+    <div className='flex flex-wrap mt-16 justify-around'>
+      {colors.length > 0 ? (
+        colors.map((color) => (
+          <div
+            onClick={() => console.log('Copy')}
+            key={Math.random()}
+            className='mx-1 mb-12 shadow-lg rounded-xl w-3/5 md:w-52 h-56 md:h-72 bg-white'
+          >
+            <div
+              className='m-2 w-50 h-40 md:h-56 rounded-md'
+              style={{ backgroundColor: `rgb(${color})` }}
+            ></div>
+            <div className='text-center font-bold uppercase p-1 text-gray-500'>
+              {`rgb (${color})`}
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>Fetching colors...</div>
+      )}
     </div>
   );
 };
