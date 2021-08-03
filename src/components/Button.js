af9a0126f34a385787dbf1d563e6b5a6d425559e
Shomari Roberts <shomariroberts@gmail.com>
2021-08-03 09:54:39 -0400

    passed generateColors function

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Button = () => {
+const Button = ({ generateColors }) => {
   return (
     <div>
-      <div className='sm:w-2/5 w-full flex justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-400 hover:bg-indigo-700 md:py-4 md:text-lg m-auto mt-8'>
+      <button
+        onClick={generateColors}
+        className='sm:w-2/5 w-full flex justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-400 hover:bg-indigo-700 md:py-4 md:text-lg m-auto mt-8'
+      >
         Generate palette
-      </div>
+      </button>
       <p className='text-center mt-6'>
         Or just press the "Spacebar" to generate new palettes.
       </p>
