2cd72c0aa96701ef2fde215c56e257dbfb842c76
Shomari Roberts <shomariroberts@gmail.com>
2021-08-16 16:14:49 -0400

    adjusted styles for margin-top

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <h1 className='text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate text-center mt-32'>
+    <h1 className='text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate text-center mt-40'>
       Color Palette Generator
     </h1>
   );
