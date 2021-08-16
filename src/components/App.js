73105e0f8ffd78ee7e635062f0d568aefd6cbdf9
Shomari Roberts <shomariroberts@gmail.com>
2021-08-16 16:12:51 -0400

    refactored api call and wired up prompt components

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,43 @@
 import React from 'react';
+import axios from 'axios';
+
 import Header from './Header';
 import ColorCard from './ColorCard';
 import Button from './Button';
+import Prompt from './Prompt';
 
 class App extends React.Component {
   state = {
     colors: [],
   };
 
-  fetchColors = () => {
-    (async () => {
-      const data = await fetch('http://colormind.io/api/', {
-        method: 'POST',
-        body: JSON.stringify({
-          model: 'default',
-        }),
+  fetchColors = async () => {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://colormind.io/api/',
+      data: JSON.stringify({
+        model: 'default',
+      }),
+    })
+      .then((res) => {
+        this.setState({ colors: res.data.result });
       })
-        .then((result) => result.json())
-        .catch(console.log);
+      .catch((err) => {
+        console.log(err);
+      });
 
-      this.setState({ colors: data.result });
-    })();
+    return res;
   };
 
   componentDidMount() {
     this.fetchColors();
 
-    window.addEventListener('keydown', this.handleKeyDown);
+    window.addEventListener('keydown', (event) => {
+      if (event.key === ' ') {
+        this.fetchColors();
+      }
+    });
   }
-
-  generateColors = () => {
-    this.fetchColors();
-  };
-
-  handleKeyDown = (event) => {
-    if (event.key === ' ') {
-      this.fetchColors();
-    }
-  };
 
   render() {
     return (
@@ -46,7 +46,17 @@ class App extends React.Component {
 
         <ColorCard colors={this.state.colors} />
 
-        <Button generateColors={this.generateColors} />
+        <Button generatePalette={this.fetchColors} />
+
+        <Prompt
+          classes='text-center mt-6'
+          message={`Or just press the "Spacebar" to generate new palettes.`}
+        />
+
+        <Prompt
+          classes='mt-12 md:w-4/12 w-full flex justify-center px-8 py-3 text-base text-center rounded-full text-black bg-white md:py-4 md:text-sm m-auto mb-40 border border-gray-200'
+          message='Click to copy individual color'
+        />
       </div>
     );
   }
