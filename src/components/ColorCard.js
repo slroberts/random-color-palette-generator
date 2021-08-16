d55ed59131abed46381f1d38beb6665b4223a7a4
Shomari Roberts <shomariroberts@gmail.com>
2021-08-16 16:20:53 -0400

    refactored and added functionality to copy individual colors

--- src/components/ColorCard.js
+++ src/components/ColorCard.js
@@ -1,28 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Clipboard from 'react-clipboard.js';
+import CopiedMessage from './CopiedMessage';
 
 const ColorCard = ({ colors }) => {
-  return (
-    <div className='flex flex-wrap mt-16 justify-around'>
-      {colors.length > 0 ? (
-        colors.map((color) => (
-          <div
-            onClick={() => console.log('Copy')}
-            key={Math.random()}
-            className='mx-1 mb-12 shadow-lg rounded-xl w-3/5 md:w-52 h-56 md:h-72 bg-white'
+  const [isCopied, setIsCopied] = useState({
+    value: '',
+    copied: false,
+  });
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsCopied({ copied: false });
+    }, 3500);
+
+    return () => clearTimeout(timeout);
+  }, [isCopied]);
+
+  const handleOnClick = (color) => {
+    setIsCopied({ value: `rgb(${color})`, copied: true });
+  };
+
+  const renderColorCard = () => {
+    return colors.map((color) => {
+      return (
+        <div
+          key={Math.random()}
+          className='mx-4 mb-12 shadow-lg rounded-xl w-3/5 md:w-52 h-56 md:h-72 bg-white cursor-pointer'
+        >
+          <Clipboard
+            data-clipboard-text={`rgb(${color})`}
+            component='a'
+            onClick={() => handleOnClick(color)}
           >
             <div
               className='m-2 w-50 h-40 md:h-56 rounded-md'
               style={{ backgroundColor: `rgb(${color})` }}
             ></div>
-            <div className='text-center font-bold uppercase p-1 text-gray-500'>
-              {`rgb (${color})`}
-            </div>
+          </Clipboard>
+
+          <div
+            id='color-text'
+            className='text-center font-bold uppercase p-1 text-gray-500'
+          >
+            {`rgb (${color})`}
           </div>
-        ))
+        </div>
+      );
+    });
+  };
+
+  return (
+    <>
+      {isCopied.copied ? <CopiedMessage isCopied={isCopied} /> : null}
+
+      {colors.length > 0 ? (
+        <div className='flex flex-wrap mt-16 justify-center'>
+          {renderColorCard()}
+        </div>
       ) : (
-        <div>Fetching colors...</div>
+        <div className='text-center mt-8'>Fetching colors...</div>
       )}
-    </div>
+    </>
   );
 };
 
